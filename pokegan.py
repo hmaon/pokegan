2db9e894572234aa5845c3f9706dc373c49c9408
Greg V <hmaon@bumba.net>
2018-10-26 03:36:19 -0400

    feature complete (maybe?) committing data in 7z

--- pokegan.py
+++ pokegan.py
@@ -11,14 +11,17 @@ from keras.layers import LeakyReLU, Dropout, ReLU
 from keras.layers import BatchNormalization
 from keras.layers import Softmax
 from keras.optimizers import Adam, RMSprop, Nadam
+from keras.callbacks import ModelCheckpoint
 
 from keras.preprocessing.image import ImageDataGenerator
 
 import keras
+import random,math
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
+import PIL
 
-EPOCHS = 25
+EPOCHS = 5000
 
 METRICS=[keras.metrics.categorical_accuracy]
 
@@ -37,7 +40,7 @@ train_datagen = ImageDataGenerator(
 train_generator = train_datagen.flow_from_directory(
         './dir_per_class',
         target_size=(96, 96),
-        batch_size=64,
+        batch_size=36,
         class_mode='binary')
 
 num_classes = train_generator.num_classes + 1 # pokemon + fake
@@ -96,42 +99,98 @@ discrim.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=METRI
 # G E N E R A T O R
 ###
 
+def g_block(model, stride=2):
+    model.add(Conv2DTranspose(32, 3, padding='same', strides=stride))
+    model.add(BatchNormalization())
+    model.add(ReLU())
+    
+
 def Generator():
     gen = keras.Sequential()
     gen.add(Dense(12*12*64, input_shape=(100+653,)))
     gen.add(Reshape(target_shape=(12,12,64)))
     gen.add(ReLU())
-    for i in range(4):
-        gen.add(Conv2DTranspose(32, 3, padding='same', strides=2))
-        gen.add(BatchNormalization())
-        gen.add(ReLU())
-    gen.add(Conv2D(3, 1))
+    for i in range(3):
+        g_block(gen)
+    g_block(gen,1)
+    gen.add(Conv2D(3, 1, activation='sigmoid'))
     gen.summary()
     return gen
 
 gen = Generator()
 gen.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=METRICS)
 
+def gen_input(_class=0):
+    noise = np.random.uniform(-1.0, 1.0, 100)
+    return np.concatenate((keras.utils.to_categorical(_class, num_classes=num_classes), noise))
+
+def gen_input_rand():
+    return gen_input(random.randint(0, num_classes-1))
+
+def gen_input_batch():
+    return np.array([gen_input_rand() for x in range(train_generator.batch_size)])
+        
+def sample(filenum=0):
+    inp = gen_input_batch()
+    print(inp.shape)
+    batch_out = gen.predict(inp)
+    print(batch_out.shape)
+    dim = math.ceil(math.sqrt(train_generator.batch_size))
+    fig, plots = plot.subplots(dim, dim)
+    plot.tight_layout()
+    plots = plots.reshape(len(plots) * len(plots[0]))
+    for i in range(len(batch_out)):
+        out = batch_out[i]
+        out = out.reshape(96, 96, 3)
+        out = np.uint8(out*255)
+        img = PIL.Image.fromarray(out)
+        plots[i].imshow(img)
+        plots[i].axis('off')
+
+    plot.savefig('out%d.png' %(filenum,))
+        
+
+#sample()
+#exit()
+
 ###
 # adversarial model
 ###
 for layer in discrim.layers:
-    layer.trainable = false
+    layer.trainable = False
 
 adver = Sequential()
 adver.add(gen)
 adver.add(discrim)
-adver.compile(loss='categorical_crossentropy', metrics=METRICS)
+adver.summary()
+adver.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=METRICS)
 
 
 ###
 # T R A I N
 ###
 
+batches = math.floor(train_generator.n / train_generator.batch_size)
+
 for epoch in range(1,EPOCHS+1):
     print("--- epoch %d ---" % (epoch,))
-    while train_generator.total_batches_seen * train_generator.batch_size < train_generator.n * epoch:
-        x,y = train_generator.next()
-        d_loss = discrim.train_on_batch(x, keras.utils.to_categorical(y, num_classes=num_classes))
+    for batch_num in range(batches):
+        # get fake data
+        x = gen.predict(gen_input_batch())
+        y = np.array([keras.utils.to_categorical(num_classes-1, num_classes = num_classes) for dummy in x])
+        # get real data
+        real_x,real_y = train_generator.next()
+        real_y = np.array([keras.utils.to_categorical(cls, num_classes=num_classes) for cls in real_y])
+        x = np.concatenate((x, real_x))
+        y = np.concatenate((y, real_y))
+        d_loss = discrim.train_on_batch(x, y)        
         print("d_loss %f, %s %f " % (d_loss[0], discrim.metrics_names[1], d_loss[1]))
+        x = gen_input_batch()
+        y = np.array([inp[:num_classes] for inp in x])
+        a_loss = adver.train_on_batch(x, y)
+        print("a_loss %f, %s %f @ %d/%d" % (a_loss[0], adver.metrics_names[1], a_loss[1], batch_num, batches))
     #train_generator.reset()
+    sample(epoch)
+    gen.save('gen-weights-%d.hdf5' % (epoch,))
+    discrim.save('discrim-weights-%d.hdf5' % (epoch,))
+    
